e481e504910cfd76cdd52d5193a50352cced4444
eugene <bantyc8@gmail.com>
2023-03-29 20:38:48 +0300

    Implemented Cli with validation to main() func

--- module.py
+++ module.py
@@ -1,5 +1,12 @@
+import argparse
 import json
 import csv
+import os
+from exceptions_for_eos_app import (
+    OutputFileExistsError,
+    FileExtensionError,
+    NotTwoDimensionalArrayError,
+)
 
 
 def save_to_json(path: str, data: any) -> None:
@@ -20,6 +27,50 @@ def save_to_csv(path: str, data: any) -> None:
             writer.writerow(row)
 
 
+def main() -> None:
+    pass
+    """Main function create cli interface witch transfer
+     arguments(path & data) to save_to_json function."""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "path",
+        type=str,
+        help="First argument - the path to the output file")
+    parser.add_argument(
+        "data",
+        type=str,
+        help="Second argument - the two-dimensional integer list as str")
+    args = parser.parse_args()
+
+    if os.path.exists(args.path):
+        """Check validation for exists of file"""
+
+        raise OutputFileExistsError("This file already exist")
+
+    data = json.loads(args.data)
+
+    if args.path.endswith(".csv"):
+        """Check validation file is in `.csv` format"""
+        save_to_csv(args.path, data)
+    elif args.path.endswith(".json"):
+        """Check validation file is in .json` format"""
+        save_to_json(args.path, data)
+    else:
+        raise FileExtensionError("File must be only in `.JSON or .CSV` format")
+
+    if not isinstance(data, list) or not all(
+            isinstance(row, list) for row in data
+    ):
+        """Check validation for two-dimensional lists of integer"""
+
+        raise NotTwoDimensionalArrayError(
+            "The data should be a two-dimensional list of integers"
+        )
+
+    save_to_json(args.path, data)
+    print(f"Create new file: {args.path}")
+
+
 if __name__ == "__main__":
-    save_to_csv()
-    save_to_json()
+    main()
